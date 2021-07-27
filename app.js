781b5c8bbfef63cba3e9c09d351f38e08b3b2d6d
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2021-07-27 16:58:37 +0200

    Minor changes

--- app.js
+++ app.js
@@ -63,19 +63,19 @@ const app = Vue.createApp({
       this.currentRound++
       const attackValue = getRandomValue(5, 12)
       this.monsterHealth -= attackValue
-      this.addLogMessage('Player', 'attack', attackValue)
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer()
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15)
       this.playerHealth -= attackValue
-      this.addLogMessage('Monster', 'attack', attackValue)
+      this.addLogMessage('monster', 'attack', attackValue)
     },
     specialAttackMonster() {
       this.currentRound++
       const attackValue = getRandomValue(10, 25)
       this.monsterHealth -= attackValue
-      this.addLogMessage('Player', 'attack', attackValue)
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer()
     },
     healPlayer() {
@@ -86,7 +86,7 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue
       }
-      this.addLogMessage('Player', 'heal', healValue)
+      this.addLogMessage('player', 'heal', healValue)
       this.attackPlayer()
     },
     surrender() {
